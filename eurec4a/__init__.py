2e895707e614a43540fbc31bbf1e6bfe11171fe1
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2022-02-08 22:58:59 +0100

    lru_cache decorator must be called as a function

--- eurec4a/__init__.py
+++ eurec4a/__init__.py
@@ -49,7 +49,7 @@ def get_meta():
                      Loader=yaml.SafeLoader)
 
 
-@lru_cache
+@lru_cache()
 def get_cids():
     return requests.get("https://raw.githubusercontent.com/eurec4a/ipfs_tools/main/cids.json").json()
 
