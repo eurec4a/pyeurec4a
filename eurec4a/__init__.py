80beefe4816c79444d5dd0f3a2bc007a62d1c285
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-03-15 00:17:03 +0100

    convert to pyproject.toml

--- eurec4a/__init__.py
+++ eurec4a/__init__.py
@@ -3,9 +3,7 @@ import intake
 import requests
 import yaml
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from ._version import __version__
 
 SEGMENT_REPO = "eurec4a/flight-phase-separation"
 
